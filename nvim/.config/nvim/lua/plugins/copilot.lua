4b5d8c8b59d174c1308170251e8f3836f59b1efb
cartogorp <duplisea.danielle@gmail.com>
2025-05-25 17:09:07 -0400

    add copilot plugin + copilot-cmp

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+-- ~/.config/nvim/lua/plugins/copilot.lua
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = true },
+        panel = { enabled = true },
+      })
+    end,
+  },
+
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
+
+
