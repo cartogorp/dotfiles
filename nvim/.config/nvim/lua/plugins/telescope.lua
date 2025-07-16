f3a4b3caff33fd076c982725ec348a33c0d09a35
cartogorp <duplisea.danielle@gmail.com>
2025-07-16 18:06:44 -0400

    nvim: hidden files visible by default telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,1 +1,23 @@
-return { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } }
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    defaults = {
+      hidden = true,
+      file_ignore_patterns = {
+        "%.git/",
+        "node_modules",
+        "%.cache",
+        "__pycache__",
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+      live_grep = {
+        hidden = true,
+      },
+    },
+  },
+}
